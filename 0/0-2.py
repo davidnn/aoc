77c5b4e21a96c36e82805d7222223ef598ec05d0
David Neikter Nilsson <david.neikter.nilsson@cloudenablers.se>
2024-11-28 13:15:24 +0100

    [2023-1] Clean up solution to part 2

--- 0/0-2.py
+++ 0/0-2.py
@@ -1,24 +1,22 @@
 from re import findall
 
 numbers_map = dict(one=1, two=2, three=3, four=4, five=5, six=6, seven=7, eight=8, nine=9)
+
+
+def match_or_map(value):
+    try:
+        return int(value)
+    except ValueError:
+        return numbers_map[value]
+
+
 total = 0
 f = open('input.txt', 'r')
 for line in f:
-    line_matches = findall('(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)
-    first_match = line_matches[0]
-    last_match = line_matches[-1]
+    line_matches = findall(r'(?=(\d|one|two|three|four|five|six|seven|eight|nine))', line)
+    first_match = match_or_map(line_matches[0])
+    last_match = match_or_map(line_matches[-1])
 
-    first_int = 0
-    try:
-        first_int = int(first_match)
-    except ValueError:
-        first_int = numbers_map[first_match]
-
-    try:
-        last_int = int(last_match)
-    except ValueError:
-        last_int = numbers_map[last_match]
-
-    total += int(str(first_int) + str(last_int))
+    total += int(str(first_match) + str(last_match))
 f.close()
 print(total)
